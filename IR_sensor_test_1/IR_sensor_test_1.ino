32bf13a767de37650209463c6e88e6dfd16916d6
FazleRabbiBindu <fazlerabbibindu@gmail.com>
2024-03-09 14:39:26 +0600

    wireless monitoring added

--- IR_sensor_test_1/IR_sensor_test_1.ino
+++ IR_sensor_test_1/IR_sensor_test_1.ino
@@ -2,16 +2,24 @@ const int signalPins[] = {A0, A1, A2, A3, A4, A5, A6, A7};
 int signalData[8];
 
 int readSensor();
-void showSignalData();
+String showSignalData();
+void sendData();
 
 void setup() {
 Serial.begin(9600);
+Serial1.begin(9600);
 }
 
 void loop() {
-  readSensor();
-  showSignalData();
-
+  sendData();
+  if(Serial1.available()>0)
+  {
+//    char data =Serial1.read();
+//    String value = String(data);
+//    Serial.print(value);
+    readSensor();
+    showSignalData(); 
+  }
 }
 
 int readSensor(){
@@ -20,12 +28,20 @@ int readSensor(){
     signalData[i] = analogRead(signalPins[i]);
   }
 }
-
-void showSignalData(){
+void sendData(){
+  String data;
+  data = showSignalData();
+  Serial1.write(data.c_str());
+}
+String showSignalData(){
+  String data = "";
   for(int i=0; i<8; i++)
   {
     Serial.print(signalData[i]);
     Serial.print(" ");
+    data = data + signalData[i]+ " ";
   }
-  Serial.println("=>");
+  Serial.println("");
+  data = data + "\n";
+  return data;
 }
