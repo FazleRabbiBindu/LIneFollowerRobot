646053d973652ff717862192408f5ab78163830d
FazleRabbiBindu <fazlerabbibindu@gmail.com>
2024-03-09 13:33:46 +0600

    read sensor, showSignalData function added. function works ok

--- IR_sensor_test_1/IR_sensor_test_1.ino
+++ IR_sensor_test_1/IR_sensor_test_1.ino
@@ -1,9 +1,31 @@
-void setup() {
-  // put your setup code here, to run once:
+const int signalPins[] = {A0, A1, A2, A3, A4, A5, A6, A7};
+int signalData[8];
 
+int readSensor();
+void showSignalData();
+
+void setup() {
+Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  readSensor();
+  showSignalData();
 
+}
+
+int readSensor(){
+  for(int i=0; i<8; i++)
+  {
+    signalData[i] = analogRead(signalPins[i]);
+  }
+}
+
+void showSignalData(){
+  for(int i=0; i<8; i++)
+  {
+    Serial.print(signalData[i]);
+    Serial.print(" ");
+  }
+  Serial.println("=>");
 }
