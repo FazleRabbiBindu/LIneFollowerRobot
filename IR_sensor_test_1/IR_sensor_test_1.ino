72e1e53f73dfd5b82802e8ff409fadfa55c4d878
FazleRabbiBindu <fazlerabbibindu@gmail.com>
2024-03-09 15:28:41 +0600

    go

--- IR_sensor_test_1/IR_sensor_test_1.ino
+++ IR_sensor_test_1/IR_sensor_test_1.ino
@@ -1,13 +1,24 @@
 const int signalPins[] = {A0, A1, A2, A3, A4, A5, A6, A7};
 int signalData[8];
 
+int motorControl[] = {2,3,4,5,9,10}; //lm1,lm2,rm1,rm2,enA,enB
+
 int readSensor();
 String showSignalData();
 void sendData();
+void Forward();
+void Backward();
+void Left();
+void Right();
+void Stop();
+
 
 void setup() {
 Serial.begin(9600);
 Serial1.begin(9600);
+  for(int i = 0; i<6; i++){
+    pinMode(motorControl[i],INPUT) 
+  }
 }
 
 void loop() {
@@ -19,8 +30,26 @@ void loop() {
 
 
 
-
-
+void Forward()
+{
+  
+}
+void Backward()
+{
+  
+}
+void Left()
+{
+  
+}
+void Right()
+{
+  
+}
+void Stop()
+{
+  
+}
 int readSensor(){
   for(int i=0; i<8; i++)
   {
